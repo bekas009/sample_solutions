7f33a4b72ae635f53a349701a6430a2181303cb5
bekas009 <76047620+bekas009@users.noreply.github.com>
2022-11-24 17:59:17 +0300

    stepik_3.3

---
+++ main.py
@@ -0,0 +1,14 @@
+# input data and split
+data = input().split()
+# sorted data
+data.sort()
+# find and sorted unique data
+unique = set(data)
+unique = sorted(unique)
+# print(data)
+for i in unique:
+    print(i, data.count(i))
+
+
+
+
